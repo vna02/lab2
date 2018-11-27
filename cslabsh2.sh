d0dba5ddf910f98268a804f1d8f65226e3e0f3c4
vna02 <42207441+vna02@users.noreply.github.com>
2018-11-27 12:48:59 +0530

    Update cslabsh2.sh

--- cslabsh2.sh
+++ cslabsh2.sh
@@ -4,9 +4,9 @@ echo "What is your name?"
 read NAME 
 echo "You are NAME."
 mkdir veena
-cd Irene2
+cd veena2
 for i in veena1.cpp veena2.cpp veena3.cpp veena4.cpp veena5.cpp
 do
-  touch "$i"
+  touch "i"
 done
 
